91b5119ff82d05be7e0e930dca3efa85476fa5a6
Sarah.Mn <sarah.mohammadian95@gmail.com>
2023-09-16 14:47:35 +0330

    login added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,70 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./auth/Login";
+import Auth from "./auth/Auth";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import Layout from "./layout/Layout";
+import Dashboard from "./pages/Dashboard";
+import RolesPage from "./pages/roles/RolesPage";
+import CategoriesPage from "./pages/categories/CategoriesPage";
+import AddCategoryPage from "./pages/categories/addCategory/AddCategory";
+import ProductsPage from "./pages/products/Products";
+import AddProductPage from "./pages/products/AddProduct";
+import PolicyPage from "./pages/policy/PolicyPage";
+import AddNewPolicyPage from "./pages/policy/addPolicy/AddNewPolicyPage";
+import UsersPage from "./pages/users/UsersPage";
+import SliderPage from "./pages/slider/SliderPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
+    </BrowserRouter> */}
+    <BrowserRouter basename={"/"}>
+      <Routes>
+        <Route path="/auth" element={<Auth />}>
+          <Route path="login" element={<Login />} />
+        </Route>
+        <Route path="/" element={<App />}>
+          <Route
+            path=""
+            element={
+              <ProtectedRoute>
+                <Layout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/roles" element={<RolesPage />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route
+              path="/categories/add-category"
+              element={<AddCategoryPage />}
+            />
+            <Route
+              path="/categories/edit-category/:id"
+              element={<AddCategoryPage />}
+            />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/add-product" element={<AddProductPage />} />
+            <Route
+              path="/products/edit-product/:id"
+              element={<AddProductPage />}
+            />
+            <Route path="/policies" element={<PolicyPage />} />
+            <Route path="/policies/add-policy" element={<AddNewPolicyPage />} />
+            <Route
+              path="/policies/edit-policy/:id"
+              element={<AddNewPolicyPage />}
+            />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/slider" element={<SliderPage />} />
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
