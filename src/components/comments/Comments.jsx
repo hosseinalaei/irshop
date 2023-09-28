b42b938a6fd2dc9febc642ecf41c9b5703cbe1e5
Sarah.Mn <sarah.mohammadian95@gmail.com>
2023-09-28 11:02:37 +0330

    comment table updated: showing just isDelete false comment

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -7,13 +7,17 @@ import ConfirmationDialog from "../common/Confirm";
 
 const Comments = () => {
   const [comments, setComments] = useState();
+  const [clickedComment, setClickedComment] = useState(null);
   const getComments = () => {
     // const body = {
     //   id: "a82d0e7b-f577-45ae-aca5-4ff3d036c628",
     // };
     axiosService.post("/Comment/getAllcomments").then((res) => {
       console.log(res);
-      setComments(res?.data);
+
+      const filteredComment = res?.data?.filter((item) => !item?.isDelete);
+
+      setComments(filteredComment);
     });
   };
 
@@ -22,75 +26,93 @@ const Comments = () => {
     getUsers();
   }, []);
 
-  const deleteComment = (comment) => {
+  const deleteComment = () => {
+    const comment = clickedComment;
+    console.log("commentcommentcommentcommentcommentcomment", comment);
     const body = {
       ...comment,
 
       isDelete: true,
     };
 
-    axiosService?.put("/Comment/updateComment", body).then((res) => {
-      if (res?.status === "Success") {
-        toast.success("عملیات با موفقیت انجام شد", {
-          position: "top-left",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          // progress: undefined,
-          theme: "light",
-          style: { fontFamily: "inherit" },
-        });
-      } else if (res?.status === "Error") {
-        toast.error("مشکلی رخ داده است", {
-          position: "top-left",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          // progress: undefined,
-          theme: "light",
-          style: { fontFamily: "inherit" },
-        });
-      }
-    });
+    axiosService
+      ?.put("/Comment/updateComment", body)
+      .then((res) => {
+        if (res?.status === "Success") {
+          toast.success("عملیات با موفقیت انجام شد", {
+            position: "top-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            // progress: undefined,
+            theme: "light",
+            style: { fontFamily: "inherit" },
+          });
+          setTimeout(() => {
+            getComments();
+          }, 500);
+        } else if (res?.status === "Error") {
+          toast.error("مشکلی رخ داده است", {
+            position: "top-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            // progress: undefined,
+            theme: "light",
+            style: { fontFamily: "inherit" },
+          });
+        }
+      })
+      .finally(() => setIsConfirmationOpen(false));
   };
-  const acceptComment = (comment) => {
+  const acceptComment = (index) => {
+    const comment = comments[index];
+
+    console.log("commentcommentcommentcommentcommentcomment", comment, index);
+
     const body = {
       ...comment,
 
       isPublish: true,
     };
 
-    axiosService?.put("/Comment/updateComment", body).then((res) => {
-      if (res?.status === "Success") {
-        toast.success("عملیات با موفقیت انجام شد", {
-          position: "top-left",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          // progress: undefined,
-          theme: "light",
-          style: { fontFamily: "inherit" },
-        });
-      } else if (res?.status === "Error") {
-        toast.error("مشکلی رخ داده است", {
-          position: "top-left",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          // progress: undefined,
-          theme: "light",
-          style: { fontFamily: "inherit" },
-        });
-      }
-    });
+    axiosService
+      ?.put("/Comment/updateComment", body)
+      .then((res) => {
+        if (res?.status === "Success") {
+          toast.success("عملیات با موفقیت انجام شد", {
+            position: "top-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            // progress: undefined,
+            theme: "light",
+            style: { fontFamily: "inherit" },
+          });
+          setTimeout(() => {
+            getComments();
+          }, 500);
+        } else if (res?.status === "Error") {
+          toast.error("مشکلی رخ داده است", {
+            position: "top-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            // progress: undefined,
+            theme: "light",
+            style: { fontFamily: "inherit" },
+          });
+        }
+      })
+      .finally(() => setIsConfirmationOpen(false));
   };
 
   const [users, setUsers] = useState([]);
@@ -116,57 +138,60 @@ const Comments = () => {
             <CommentsListHead />
             <tbody>
               {comments?.map((comment, index) => {
-                if (!comment?.isDelete) {
-                  const user = users?.filter(
-                    (item) => item?.id === comment?.userId
-                  );
+                // if (!comment?.isDelete) {
+                const user = users?.filter(
+                  (item) => item?.id === comment?.userId
+                );
 
-                  return (
-                    <tr key={index}>
-                      <td class="flex justify-center">
-                        {comment?.isPublish ? (
-                          <div className="w-6 h-6 bg-green-500 rounded-full"></div>
-                        ) : (
-                          <div className="w-6 h-6 bg-red-500 rounded-full"></div>
-                        )}
-                      </td>
-                      <td class="fw-bold text-right">
-                        {user[0]?.firstName} {user[0]?.lastName}
-                      </td>
-                      <td class="text-center text-gray-600 fw-bold">
-                        {comment?.text}
-                      </td>
-                      <td class="text-center fw-semibold text-muted">
-                        {convertToPersianDate(comment?.createDate)}
-                      </td>
-                      <td className="text-center">
-                        <div className="flex">
-                          <button
-                            className="px-3 "
-                            onClick={() => acceptComment(comment)}
-                          >
-                            تایید
-                          </button>
-                          <button
-                            className="px-3 "
-                            onClick={() => setIsConfirmationOpen(true)}
-                          >
-                            حذف
-                          </button>
-                        </div>
-                      </td>
+                return (
+                  <tr key={index}>
+                    <td class="flex justify-center">
+                      {comment?.isPublish ? (
+                        <div className="w-6 h-6 bg-green-500 rounded-full"></div>
+                      ) : (
+                        <div className="w-6 h-6 bg-red-500 rounded-full"></div>
+                      )}
+                    </td>
+                    <td class="fw-bold text-right">
+                      {user[0]?.firstName} {user[0]?.lastName}
+                    </td>
+                    <td class="text-center text-gray-600 fw-bold">
+                      {comment?.text}
+                    </td>
+                    <td class="text-center fw-semibold text-muted">
+                      {convertToPersianDate(comment?.createDate)}
+                    </td>
+                    <td className="text-center">
+                      <div className="flex">
+                        <button
+                          className="px-3 "
+                          onClick={() => acceptComment(index)}
+                        >
+                          تایید
+                        </button>
+                        <button
+                          className="px-3 "
+                          onClick={() => {
+                            setClickedComment(comment);
+                            setIsConfirmationOpen(true);
+                          }}
+                        >
+                          حذف
+                        </button>
+                      </div>
                       <ConfirmationDialog
                         isOpen={isConfirmationOpen}
                         setIsOpen={setIsConfirmationOpen}
                         message="از حذف نظر اطمینان دارید؟"
-                        onConfirm={() => deleteComment(comment)}
+                        onConfirm={() => deleteComment()}
                         onCancel={handleCancel}
                         confirmText="بله"
                         cancelText="خیر"
                       />
-                    </tr>
-                  );
-                }
+                    </td>
+                  </tr>
+                );
+                // }
               })}
             </tbody>
           </table>
