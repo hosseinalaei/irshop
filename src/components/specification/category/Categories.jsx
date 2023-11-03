d7e8eba7c3deb2b3eed21151781557f19d0c5d3e
Sarah.Mn <sarah.mohammadian95@gmail.com>
2023-11-03 13:29:39 +0330

    category added

---
+++ src/components/specification/category/Categories.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import Button from "../../common/Button";
+import GroupsList from "./CategoriesList";
+import Loading from "../../common/Loading";
+import { ToastContainer } from "react-toastify";
+import { NavLink, useNavigate } from "react-router-dom";
+import useAxios from "../../../hooks/useAxios";
+import { useState } from "react";
+import { useEffect } from "react";
+import CategoriesList from "./CategoriesList";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  const [groups, setGroups] = useState([]);
+
+  const httpRequest = useAxios();
+
+  const getCategorySpecs = () => {
+    // setLoading(true);
+    httpRequest({
+      url: "/Specification/getAllAttributeCategory",
+      method: "GET",
+    }).then((res) => {
+      setCategories(res?.data);
+      // setLoading(false);
+    });
+  };
+  const getGroups = () => {
+    // setLoading(true);
+    // axiosService.get("/Specification/getAllSpecs")
+    httpRequest({
+      url: "/Specification/getAllAttributeGroup",
+      method: "GET",
+    }).then((res) => {
+      setGroups(res?.data);
+      // setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getCategorySpecs();
+    getGroups();
+  }, []);
+
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    const valuesExp = categories?.reduce((result, category) => {
+      const categoryObject = { ...category, group: null };
+
+      categoryObject.group = category?.attributeGroupsId
+        ?.map((item) => groups?.find((spec) => spec?.id === item))
+        .filter((item) => item);
+
+      if (categoryObject?.group?.length > 0) {
+        result.push(categoryObject);
+      }
+
+      return result;
+    }, []);
+    setValues(valuesExp);
+    // console.log("valuesvaluesvaluesvaluesvaluesvaluesvalues", values);
+  }, [categories, groups]);
+
+  const nav = useNavigate();
+  return (
+    <>
+      {/* {loading ? (
+        <Loading />
+      ) : ( */}
+      <>
+        <ToastContainer />
+        <div className="flex-column-fluid">
+          <div className="container-xxl">
+            <div className="m-3 page-title d-flex flex-column justify-content-center">
+              <h1 className="my-0 page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center">
+                لیست محصولات
+              </h1>
+
+              <ul className="pt-1 my-0 breadcrumb breadcrumb-separatorless fw-semibold fs-7">
+                <NavLink
+                  to="/"
+                  className="text-lg font-bold text-black hover:text-red-500"
+                >
+                  صفحه اصلی
+                </NavLink>
+
+                <li className="breadcrumb-item">/</li>
+                <li className="text-lg font-bold text-black hover:text-red-500">
+                  محصولات
+                </li>
+
+                {/* <li className="breadcrumb-item text-muted">نقش‌ها</li> */}
+              </ul>
+            </div>
+            <div className="card card-flush">
+              <div className="gap-2 py-5 card-header align-items-center gap-md-5">
+                <div className="card-title"></div>
+                <div className="card-toolbar">
+                  <Button
+                    onClick={() =>
+                      nav("/admin/specification/categories/add-category")
+                    }
+                  >
+                    ویژگی جدید
+                  </Button>
+                </div>
+              </div>
+              <CategoriesList
+                categories={categories}
+                values={values}
+                getCategories={getCategorySpecs}
+                // getProducts={getGroups}
+              />
+            </div>
+          </div>
+        </div>
+      </>
+      {/* )} */}
+    </>
+  );
+};
+
+export default Categories;
---
+++ src/components/specification/category/CategoriesList.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import GroupsListHead from "./CategoriesListHead";
+import { CategoriesListItem, GroupsListItem } from "./CategoriesListItem";
+import Pagination from "../../common/Pagination";
+import CategoriesListHead from "./CategoriesListHead";
+
+const CategoriesList = ({ categories, values, getCategories }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = values?.slice(indexOfFirstItem, indexOfLastItem);
+
+  return (
+    <div className="card card-flush">
+      <div className="pt-0 overflow-scroll card-body">
+        {/* <!--begin::Table--> */}
+        <table
+          className="table align-middle table-row-dashed fs-6 gy-5"
+          id="kt_ecommerce_category_table"
+        >
+          <CategoriesListHead />
+          <tbody className="text-gray-600 fw-semibold">
+            {currentItems?.map((item, index) => (
+              <CategoriesListItem
+                key={index}
+                category={item}
+                getCategories={getCategories}
+                // getSpecs={getSpecs}
+              />
+            ))}
+            {/* <ProductsListItem />
+    <ProductsListItem />
+    <ProductsListItem /> */}
+          </tbody>
+          {/* <!--end::Table body--> */}
+        </table>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(categories?.length / itemsPerPage)}
+          onPageChange={handlePageChange}
+        />
+        {/* <!--end::Table--> */}
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesList;
